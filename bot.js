449c5641af630c1275902cb455868ef8a1c79b79
caio12131 <caio.caca100@gmail.com>
2025-01-31 10:54:46 -0300

    teste

---
+++ bot.js
@@ -0,0 +1,68 @@
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+
+// Criar cliente do WhatsApp Web.js
+const client = new Client({
+    authStrategy: new LocalAuth() // Salva a sessão localmente
+});
+
+// Gera QR Code no terminal
+client.on('qr', (qr) => {
+    console.log('Escaneie o QR Code abaixo para conectar:');
+    qrcode.generate(qr, { small: true });
+});
+
+// Confirma que o bot está conectado
+client.on('ready', () => {
+    console.log('Bot do WhatsApp está online!');
+});
+
+// Detecta mensagens recebidas
+client.on('message', async (message) => {
+    console.log(`Mensagem recebida de ${message.from}: ${message.body}`); // Log da mensagem recebida
+    
+    const agora = new Date();
+    const hora = agora.getHours();
+
+    // Verifica se está no intervalo de 18h às 8h
+    if (hora >= 18 || hora < 10) {
+        const delay = Math.floor(Math.random() * (10000 - 5000 + 1)) + 5000; // Delay de 5 a 10 segundos
+
+        setTimeout(async () => {
+            try {
+                const audioPath = 'C:\\Users\\caioc\\OneDrive\\Documentos\\botWpp\\audio.mp3'; // Caminho absoluto do arquivo de áudio
+                console.log(`Verificando se o arquivo de áudio existe em: ${audioPath}`);
+                
+                // Verificar se o arquivo de áudio existe
+                if (!fs.existsSync(audioPath)) {
+                    console.error('Erro: Arquivo de áudio não encontrado!');
+                    return;
+                }
+        
+                console.log(`Arquivo de áudio encontrado. Enviando áudio para ${message.from} em ${delay / 1000} segundos...`);
+        
+                // Usando MessageMedia.fromFilePath para carregar o áudio
+                const media = MessageMedia.fromFilePath(audioPath);
+                
+                console.log('Áudio carregado com sucesso. Enviando...');
+        
+                // Enviar áudio
+                await client.sendMessage(message.from, media, { sendAudioAsVoice: true });
+                console.log(`Áudio enviado para ${message.from}`);
+            } catch (error) {
+                console.error(`Erro ao enviar áudio: ${error.message}`);
+                console.error(error.stack); // Log completo do erro para rastrear a origem
+            }
+        }, delay);
+        
+    }
+});
+
+// Testa envio de mensagem de texto
+client.on('message', async (message) => {
+    await client.sendMessage(message.from, "Mensagem Automática Enviada");
+});
+
+// Inicia o bot
+client.initialize();
